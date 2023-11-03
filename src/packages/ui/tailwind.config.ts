2432db1913cc643e004ec992afdb89810fc745ee
John P <johnrprutton@gmail.com>
2023-11-03 14:59:38 +0200

    feat(`ui`): setup tailwind to work across projects

--- src/packages/ui/tailwind.config.ts
+++ src/packages/ui/tailwind.config.ts
@@ -1,6 +1,8 @@
+import { join } from "path";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [] as string[],
+  content: [join(__filename, "../src") + "/**/*.tsx"] as string[],
   theme: {
     extend: {
       backgroundImage: {
