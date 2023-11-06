a3493bf0f8f034759b4e7d114f13c9a2d88d72dc
John P <johnrprutton@gmail.com>
2023-11-06 16:30:55 +0200

    update tsconfig, use env vars

--- src/packages/database/drizzle.config.ts
+++ src/packages/database/drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "drizzle-kit";
+import { DATABASE_URL } from "./src/env";
 
 export default {
   schema: "./src/schema.ts",
   out: "./src/migrations",
   driver: "libsql",
   dbCredentials: {
-    url: "local.db",
+    url: DATABASE_URL,
   },
 } satisfies Config;
