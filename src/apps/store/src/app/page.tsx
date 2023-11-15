344747c90902a50a53ee28dda5374496dc07efb4
John P <johnrprutton@gmail.com>
2023-11-15 22:04:46 +0200

    add insta image

--- src/apps/store/src/app/page.tsx
+++ src/apps/store/src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Hero } from "src/components/layout/hero"
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
+    <main>
+      <Hero />
+    </main>
   )
 }
