3680deb8d3a4111e13777bdccac27e278c7e940a
John P <johnrprutton@gmail.com>
2023-11-19 22:09:56 +0200

    feat: add search drawer

--- src/apps/store/src/components/layout/navbar.tsx
+++ src/apps/store/src/components/layout/navbar.tsx
@@ -1,17 +1,9 @@
-import Link from "next/link"
-
-import { MenuIcon, SearchIcon } from "ui/src/icons"
-import { Button } from "ui/src/ui/button"
-
-import { getUserAuth } from "@/lib/auth"
-
 import { Cart } from "./cart"
 import { Logo } from "./logo"
 import { Menu } from "./menu"
+import { Search } from "./search"
 
-export async function Navbar() {
-  const session = await getUserAuth()
-
+export function Navbar() {
   return (
     <nav className="bg-background/30 sticky top-0 z-10 grid grid-cols-3 p-3 backdrop-blur">
       <Menu />
@@ -19,8 +11,8 @@ export async function Navbar() {
       <Logo className="" />
 
       <div className="ml-auto flex w-fit flex-row items-center gap-1">
-        <SearchIcon />
-        <Cart items={session?.user ? 1 : 0} />
+        <Search />
+        <Cart />
       </div>
     </nav>
   )
