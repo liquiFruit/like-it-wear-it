5fa5b23f7240a35e0987305caa959e8329ce0298
John P <johnrprutton@gmail.com>
2023-11-20 23:39:10 +0200

    feat: call db from trpc endpoint

--- src/apps/store/src/server/index.ts
+++ src/apps/store/src/server/index.ts
@@ -1,7 +1,9 @@
+import { getAllProductsInStock } from "database/src/api/products/queries"
+
 import { publicProcedure, router } from "@/server/trpc"
 
 export const appRouter = router({
-  test: publicProcedure.query(async () => "greetings"),
+  test: publicProcedure.query(getAllProductsInStock),
 })
 
 export type AppRouter = typeof appRouter
