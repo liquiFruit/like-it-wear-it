fa6fbb97756ecb9f16bc7880668515c7efc03ca0
John P <johnrprutton@gmail.com>
2023-11-20 23:56:40 +0200

    chore: clean up tut files

--- src/apps/store/src/components/trpc-server-caller.tsx
+++
@@ -1,7 +0,0 @@
-import { serverClient } from "@/lib/trpc/server-client"
-
-export async function TRPCServerCaller() {
-  const data = await serverClient.test()
-
-  return <>{JSON.stringify(data)}</>
-}
