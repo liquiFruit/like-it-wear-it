c678dc9d13f1f3d559a3a8104f305abc574862f8
John P <johnrprutton@gmail.com>
2023-11-22 00:08:24 +0200

    feat: read products in from local storage when logged out

--- src/apps/store/src/lib/hooks/useCart.ts
+++ src/apps/store/src/lib/hooks/useCart.ts
@@ -1,10 +1,31 @@
 "use client"
 
+import { useEffect } from "react"
+
+import { Select as Product } from "database/src/schema/products"
+
 import { trpc } from "@/lib/trpc/client"
 
 export function useCart() {
   const { data, isLoading, isError, error, refetch } =
     trpc.getCartProductsByUserId.useQuery()
+
+  const { getData, setData } = trpc.useUtils().getCartProductsByUserId
+
+  useEffect(() => {
+    if (!isError || error.data?.code !== "UNAUTHORIZED") return
+
+    const localProducts = JSON.parse(
+      localStorage.getItem("local_cart_products") ?? "[]",
+    ) as Product[]
+
+    setData(
+      undefined,
+      localProducts.map((p) => ({ ...p, createdAt: p.createdAt.toString() })),
+    )
+
+    console.log("setData")
+  }, [isError, error, data])
 
   return { data, isLoading, isError, error }
 }
