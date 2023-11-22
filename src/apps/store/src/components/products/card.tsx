251e9bc55d0a319d1fe8a2ea28563711e874a372
John P <johnrprutton@gmail.com>
2023-11-22 16:50:52 +0200

    style up: product card

--- src/apps/store/src/components/products/card.tsx
+++ src/apps/store/src/components/products/card.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image"
 
 import { schema } from "database"
-import { Heart } from "ui/src/svgs"
 
 export function ProductCard({
   product: { name, description, price, images },
@@ -14,12 +13,10 @@ export function ProductCard({
         <Image src={images[0]} alt="" fill className="object-cover" />
       </div>
 
-      <div className="font-hand relative text-center">
-        <h3 className="font-bold">{name}</h3>
+      <div className="font-hand text-center">
+        <h3 className="truncate font-bold">{name}</h3>
         <p className="truncate text-xs">{description}</p>
         <span className="text-primary font-black">R{price / 100}</span>
-
-        <Heart className="absolute bottom-0 right-0 translate-x-1 translate-y-1 rotate-12" />
       </div>
     </div>
   )
